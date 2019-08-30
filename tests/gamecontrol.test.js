ddcb37f34e07e07bc40038b7bde177c49bb6f8e1
Alvaro Montoro <alvaromontoro@gmail.com>
2019-08-30 16:01:06 -0500

    Add axeThreshold tests
    - Add axeThreshold tests for gamepad and gameControl

--- tests/gamecontrol.test.js
+++ tests/gamecontrol.test.js
@@ -6,6 +6,7 @@ function generateGamepads() {
   const auxGamepads = {};
   for (let x = 0; x < gamepads.length; x++) {
     auxGamepads[x] = gamepad.init(gamepads[x]);
+    auxGamepads[x].set('axeThreshold', gameControl.axeThreshold);
   }
   gameControl.gamepads = auxGamepads;
 }
@@ -28,5 +29,24 @@ describe('gameControl', () => {
     const gp = gameControl.getGamepad(0);
     expect(gp.id).toEqual(0);
     expect(gp.mapping).toEqual('standard');
+  });
+
+  test('Verify sensitivity threshold', () => {
+    generateGamepads();
+    const gp = gameControl.getGamepad(0);
+    expect(gp.axeThreshold[0]).toEqual(1.0);
+    gp.set('axeThreshold', [0.3]);
+    expect(gp.axeThreshold[0]).toEqual(0.3);
+  });
+
+  test('Verify sensitivity threshold', () => {
+    gameControl.axeThreshold = [0.5];
+    generateGamepads();
+    const gp = gameControl.getGamepad(0);
+    expect(gp.axeThreshold[0]).toEqual(0.5);
+    gp.set('axeThreshold', [0.3]);
+    expect(gp.axeThreshold[0]).toEqual(0.3);
+    gameControl.axeThreshold = [0.5];
+    expect(gp.axeThreshold[0]).toEqual(0.3);
   });
 });
