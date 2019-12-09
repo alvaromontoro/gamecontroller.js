bbd256545bdf254647e283775950a8d4685ecbee
Alvaro Montoro <alvaromontoro@gmail.com>
2019-12-08 19:47:18 -0600

    Add tests
    - Add tests to tools
    - Bump version

--- tests/tools.test.js
+++ tests/tools.test.js
@@ -1,4 +1,5 @@
 import { isGamepadSupported, log, error, emptyEvents } from '../src/tools';
+import { gamepads } from './mock.gamepads';
 
 describe('log', () => {
   it('log works without parameters', () => {
@@ -13,11 +14,20 @@ describe('log', () => {
     log('Info sentence test', 'info');
     log('Log sentence test', 'log');
   });
-});
 
-describe('log', () => {
   it('error log works', () => {
     error('Error sentence test');
+  });
+
+  it('branch test console and error', () => {
+    const auxconsole = console;
+    console = null;
+    log('Console sentence test');
+    log('Error sentence test', 'error');
+    log('Info sentence test', 'info');
+    log('Info sentence test', 'log');
+    error('Error sentence test');
+    console = auxconsole;
   });
 });
 
@@ -25,6 +35,28 @@ describe('isGamepadSupported', () => {
   it('check if gamepad is supported', () => {
     const aux = isGamepadSupported();
     expect(aux).toEqual(false);
+  });
+
+  it('check if gamepad is supported', () => {
+    const mockGamepads = () => gamepads;
+    global.navigator.getGamepads = mockGamepads;
+    const aux1 = isGamepadSupported();
+    expect(aux1).toEqual(true);
+
+    global.navigator.getGamepads = 'error';
+    const aux2 = isGamepadSupported();
+    expect(aux2).toEqual(false);
+  });
+
+  it('check if gamepad is supported (webkit)', () => {
+    const webkitGetGamepads = () => gamepads;
+    global.navigator.webkitGetGamepads = webkitGetGamepads;
+    const aux = isGamepadSupported();
+    expect(aux).toEqual(true);
+
+    global.navigator.webkitGetGamepads = 'error';
+    const aux2 = isGamepadSupported();
+    expect(aux2).toEqual(false);
   });
 });
 
